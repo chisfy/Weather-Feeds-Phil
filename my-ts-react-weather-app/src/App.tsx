ed8e5f8b04e0065c6518ae853ba8c422544e376e
chisfy <fyzac96@gmail.com>
2024-01-16 17:55:27 +0000

    first line

--- my-ts-react-weather-app/src/App.tsx
+++ my-ts-react-weather-app/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Hello name="TypeScript React" />
+        <Hello name="Fyza" />
       </header>
     </div>
   );
