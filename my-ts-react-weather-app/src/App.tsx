c90eac9db8697391fbc039cf88aa585f6349bf90
chisfy <fyzac96@gmail.com>
2024-01-16 17:23:20 +0000

    typescript set up

---
+++ my-ts-react-weather-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Hello from  './components/Hello.tsx'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Hello name="TypeScript React" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
