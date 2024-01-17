1c7904626018d3ea73e6b007ecda924b8ee27a49
chisfy <fyzac96@gmail.com>
2024-01-17 15:28:03 +0000

    logo added

--- my-ts-react-weather-app/src/App.tsx
+++ my-ts-react-weather-app/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Time from  './components/Time.tsx'
+import Logo from './components/Logo.tsx'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Logo />
         <Time />
       </header>
     </div>
