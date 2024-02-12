8e91f41564f0acd1ade659123f898fdacadd789c
chisfy <fyzac96@gmail.com>
2024-02-12 21:21:50 +0000

    starting to do bascic render test

--- my-ts-react-weather-app/src/App.test.tsx
+++ my-ts-react-weather-app/src/App.test.tsx
@@ -1,10 +1,29 @@
+import React from "react"
 import { render, screen } from "@testing-library/react"
 import App from "./App"
+import { LocationContext } from "./context/locationbuttoncontext"
+
+const mockContextValue = {
+    locations: [],
+    setLocations: jest.fn(),
+    longitude: 0,
+    setLongitude: jest.fn(),
+    latitude: 0,
+    setLatitude: jest.fn(),
+    showCard: false,
+    setShowCard: jest.fn(),
+    index: 0,
+    setIndex: jest.fn()
+}
 
 test("basic test", () => {
 
 })
 
-test("basic 2 test", () => {
-
+test("checking basic render of the page", async () => {
+    render(
+    <LocationContext.Provider value={mockContextValue}>
+        <App />
+    </LocationContext.Provider>
+    );
 })
